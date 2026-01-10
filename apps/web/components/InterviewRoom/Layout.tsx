e22908e6d3d3830ff839c223c505d82fce46766c
ethanchiou <ethanchiouu@gmail.com>
2026-01-10 18:10:52 -0500

    fixed div to look neater, added interviewer icon

--- apps/web/components/InterviewRoom/Layout.tsx
+++ apps/web/components/InterviewRoom/Layout.tsx
@@ -38,20 +38,25 @@ export default function Layout({
     onRunResult,
     sttFallback,
 }: LayoutProps) {
+    const divStyle = {
+        borderRadius: "30px",
+        overflow: "hidden",
+        margin: "3px",
+    };
     return (
-        <div className="h-screen flex flex-col">
+        <div className="h-screen flex flex-col" style={divStyle}>
             {/* Main content area */}
-            <div className="flex-1 flex overflow-hidden">
+            <div className="flex-1 flex overflow-hidden" style={divStyle}>
                 {/* Left Column (50%) */}
-                <div className="w-2/5 h-full flex flex-col border-r border-gray-200 dark:border-gray-700">
+                <div className="w-2/5 h-full flex flex-col border-r border-gray-200 dark:border-gray-700" style={divStyle}>
                     {/* Top-Left: Interviewer (50%) */}
-                    <div className="flex-1 min-h-0 border-t border-gray-200 dark:border-gray-700">
+                    <div className="flex-1 min-h-0 border-t border-gray-200 dark:border-gray-700" style={divStyle}>
                         {/* Replaced CoachPanel with WebcamPanel */}
                         <WebcamPanel />
                     </div>
 
                     {/* Bottom-Left: Webcam (50%) */}
-                    <div className="flex-1 min-h-0">
+                    <div className="flex-1 min-h-0" style={divStyle}>
                         <InterviewerPanel
                             messages={interviewerMessages}
                             startTime={startTime}
@@ -60,9 +65,9 @@ export default function Layout({
                 </div>
 
                 {/* Right Column (50%) */}
-                <div className="w-3/5 h-full flex flex-col">
+                <div className="w-3/5 h-full flex flex-col" style={divStyle}>
                     {/* Top-Right: Editor (66% -> Flex-[2]) */}
-                    <div className="flex-[2] min-h-0">
+                    <div className="flex-[2] min-h-0" style={divStyle}>
                         <EditorPanel
                             question={question}
                             code={code}
@@ -72,7 +77,7 @@ export default function Layout({
                     </div>
 
                     {/* Bottom-Right: Transcript (33% -> Flex-[1]) */}
-                    <div className="flex-[1] min-h-0 border-t border-gray-200 dark:border-gray-700">
+                    <div className="flex-[1] min-h-0 border-t border-gray-200 dark:border-gray-700" style={divStyle}>
                         <TranscriptPanel
                             entries={transcriptEntries}
                             currentPartial={currentPartial}
@@ -83,7 +88,7 @@ export default function Layout({
 
             {/* STT fallback (if provided) */}
             {sttFallback && (
-                <div className="bg-yellow-100 dark:bg-yellow-900 p-3 border-t border-yellow-300 dark:border-yellow-700">
+                <div className="bg-yellow-100 dark:bg-yellow-900 p-3 border-t border-yellow-300 dark:border-yellow-700" style={divStyle}>
                     {sttFallback}
                 </div>
             )}
